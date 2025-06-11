a814ab7061506ed0e6134a458892455a480326b9
EmmaKumlien <ems.93@hotmail.com>
2025-06-11 11:46:51 +0200

    Fix drop down menu for mobile

--- JavaScript/menuDropDown.js
+++ JavaScript/menuDropDown.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuBar = document.getElementById("menuBar");
     const navigationLinks = document.querySelector(".navigationLinks");
+    const links = navigationLinks.querySelectorAll("a");
 
-    menuBar.addEventListener("click", () => {
-        navigationLinks.classList.toggle("active");
+    if (menuBar) {
+        console.log("menuBar found");
+        menuBar.addEventListener("click", () => {
+            console.log("menuBar clicked");
+            navigationLinks.classList.toggle("active");
+        });
+    } else {
+        console.error("menuBar not found");
+    }
+
+    links.forEach(link => {
+        link.addEventListener("click", () => {
+            console.log("Link clicked");
+            navigationLinks.classList.remove("active");
+        });
     });
 });
