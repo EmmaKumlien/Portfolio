2661a505336e98184262432bfa86d570e386c073
EmmaKumlien <ems.93@hotmail.com>
2025-06-13 18:37:49 +0200

    Update logo for c#

--- JavaScript/setCSharpLogo.js
+++ JavaScript/setCSharpLogo.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const cSharpLogo = document.getElementById("cSharp-logo");
     if (cSharpLogo) {
         console.log("C# logo element found");
-        const imagePath = `${window.location.origin}/Images/CSharp_logo.png`; // Dynamic path
+        const imagePath = "./Images/CSharp_logo.png";
         cSharpLogo.style.backgroundImage = `url("${imagePath}")`;
         cSharpLogo.style.backgroundSize = "contain";
         cSharpLogo.style.backgroundRepeat = "no-repeat";
     } else {
-        console.warn("C# logo element not found");
+        console.log("C# logo element not found");
     }
 });
