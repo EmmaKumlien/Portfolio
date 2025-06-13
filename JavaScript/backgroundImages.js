8f4678e9581406a8f3f4696a2610aab19d0a8b6b
EmmaKumlien <ems.93@hotmail.com>
2025-06-13 16:11:45 +0200

    Try javascript for background image

---
+++ JavaScript/backgroundImages.js
@@ -0,0 +1,2 @@
+document.querySelector('.hero-section').style.backgroundImage = "url('./Images/BG_Contact.png')";
+document.querySelector('.contact').style.backgroundImage = "url('./Images/BG_Contact.png')";
