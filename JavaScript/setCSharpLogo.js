6495d836c287dcdcd2eed877dc774a1570ae76ba
EmmaKumlien <ems.93@hotmail.com>
2025-06-13 18:44:47 +0200

    Try new path for C# logo

--- JavaScript/setCSharpLogo.js
+++ JavaScript/setCSharpLogo.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cSharpLogo = document.getElementById("cSharp-logo");
     if (cSharpLogo) {
         console.log("C# logo element found");
-        const imagePath = "/Images/CSharp_logo.png";
-        cSharpLogo.style.backgroundImage = `url("${imagePath}")`;
+        cSharpLogo.style.backgroundImage = `url("./Images/CSharp_logo.png")`;
         cSharpLogo.style.backgroundSize = "contain";
         cSharpLogo.style.backgroundRepeat = "no-repeat";
     } else {
